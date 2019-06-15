96c741e7525cd564b4f2dc147b31d0ddf82911c9
Afonso Neto <afonso.pontesneto@gmail.com>
2019-06-15 17:08:52 -0300

    [Upload] Implementing upload for multiple files...

--- app/assets/javascripts/uploads.js
+++ app/assets/javascripts/uploads.js
@@ -1,2 +1,16 @@
-// Place all the behaviors and hooks related to the matching controller here.
-// All this logic will automatically be available in application.js.
+var Upload = (function($){
+  var _formSelector = "form#upload_inp_files_form";
+
+  function _init() {
+    Dropzone.autoDiscover = false;
+
+    $(_formSelector).dropzone({
+      paramName: "upload[file]",
+      success: function(file, response){
+        alert('Your file is being processed...');
+      }
+    });
+  }
+
+  return { init: _init }
+}($));
--- app/controllers/uploads_controller.rb
+++ app/controllers/uploads_controller.rb
@@ -4,6 +4,18 @@ class UploadsController < ApplicationController
   end
 
   def create
-    render json: { message: "success", fileId: :whatever }, status: :ok
+    @upload = Upload.new(upload_params)
+
+    if @upload.save
+      render json: { message: "success", fileId: :whatever }, status: :ok
+    else
+      render json: { error: @upload.errors.full_messages.join(',')}, status: :unprocessable_entity
+    end
   end
+
+  private
+
+    def upload_params
+      params.require(:upload).permit(:file)
+    end
 end
