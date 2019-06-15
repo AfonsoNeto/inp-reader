467d678ad7cc9fe8c73266cdd6b8fc5a0732450c
Afonso Neto <afonso.pontesneto@gmail.com>
2019-06-15 17:48:25 -0300

    [Inp] Adding const with known Inp object types

--- app/models/inp.rb
+++ app/models/inp.rb
@@ -1,2 +1,16 @@
 class Inp < ApplicationRecord
+  KNOWN_TYPES = [
+    "COMPLIANCE",
+    "CONSTRUCTION",
+    "GLASS",
+    "DHW-GENERATOR",
+    "HVAC-SYSTEM",
+    "ZONE",
+    "WALL",
+    "WINDOW",
+    "DOOR",
+    "REC-USER",
+    "GENERAL",
+    "REC-PROJECT"
+  ]
 end
