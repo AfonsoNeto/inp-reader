f3a5f18232a618ef9d0fb9a67f02b8c628bfecc7
Afonso Neto <afonso.pontesneto@gmail.com>
2019-06-17 00:06:47 -0300

    [Redis] Locking redis gem version in a one that works with Rails 5.2

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,2 +1,3 @@
 redis_uri = ENV.fetch("REDISTOGO_URL", "redis://localhost:6379")
-REDIS = Redis.new(url: redis_uri)
+redis_client = Redis.new(url: redis_uri)
+REDIS = Redis::Namespace.new("inp_reader", redis: redis_client)
