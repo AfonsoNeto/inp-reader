eaf16ad27c36bd50de2e466ae061710ca0c52a34
Afonso Neto <afonso.pontesneto@gmail.com>
2019-06-17 05:15:10 -0300

    [Mailer] Avoiding giant email bodies

--- app/mailers/inp_mailer.rb
+++ app/mailers/inp_mailer.rb
@@ -1,7 +1,7 @@
 class InpMailer < ApplicationMailer
   def notify
     @upload = Upload.find(params[:upload_id])
-    @result = JSON.parse REDIS.get(@upload.id)
+    @results = JSON.parse(REDIS.get(@upload.id))[@upload.id.to_s]
     mail(to: @upload.recipient, subject: 'Inp file processing results')
   end
 end
--- app/workers/inp_worker.rb
+++ app/workers/inp_worker.rb
@@ -6,6 +6,6 @@ class InpWorker
     parser = Parser.new(file: upload.read_file_from_s3, upload_id: upload_id)
     parser.parse_while_storing_on_redis!
 
-    InpMailer.with(upload_id: upload_id).notify.deliver_later
+    InpMailer.with(upload_id: upload_id).notify.deliver_now
   end
 end
